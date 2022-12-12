8708fbbb65a6b5217dc25286c7efbcf1c9b69285
Amasterworld <73497298+Amasterworld@users.noreply.github.com>
2022-12-12 11:31:50 +0100

    add comment TC and SC

--- 224_MinimumRoundstoCompleteAllTasks.py
+++ 224_MinimumRoundstoCompleteAllTasks.py
@@ -1,5 +1,8 @@
+#TC is O(n)
+#SC also is O(n);
+from collections import Counter
 class Solution:
-    def minimumRound(self, tasks : list([int])) ->int:
+    def minimumRounds(self, tasks : list([int])) ->int:
         
         min_round = 0;
         freq_num = Counter(tasks)
@@ -9,6 +12,14 @@ class Solution:
                 return -1;
             
             else:
-                min_round = freq//3 if freq % 3 == 0 else freq//3 +1;
-                # min_round = freq%3 == 0 ? freq/3 : freq/3 +1; C++
+                min_round += freq//3 if freq % 3 == 0 else freq//3 +1;
+                # min_round += freq%3 == 0 ? freq/3 : freq/3 +1; C++
         return min_round;
+
+if __name__ == '__main__':
+    
+    myObj = Solution();
+    tasks_1 = [2,2,3,3,2,4,4,4,4,4]
+    tasks_2 = [2,3,3]
+    print(myObj.minimumRounds(tasks_1));
+    print(myObj.minimumRounds(tasks_2));
